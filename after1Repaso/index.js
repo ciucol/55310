6129db51de71e4c0c19bc869b80597bd57be7ed7
Diego Naranjo <contact@diegonaranjo.co>
2023-07-19 16:48:41 -0500

    clase6

--- after1Repaso/index.js
+++ after1Repaso/index.js
@@ -1,2 +1,20 @@
-console.log('hola desde un editor')
-console.log('hola desde un editor de terminal')
+const fs = require('fs')
+
+fs.writeFileSync('./miArchivo.txt', 'hola desde callback')
+
+fs.readFile('./miArchivo.txt', 'utf-8', (err, data) => {
+  if (err) return console.log(err)
+
+  console.log(data)
+})
+
+const func = async () => {
+  try {
+    const response = await fs.promises.unlink('./miArchivo.txt')
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+func()
