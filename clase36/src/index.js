3964076f5d1a62066ab12ff4b2bd9a4d28ea1c27
Diego Naranjo <contact@diegonaranjo.co>
2023-11-14 18:46:36 -0500

    clase36

--- clase36/src/index.js
+++ clase36/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(morgan('combined'))
 
 app.get('/users', (req, res) => {
-  res.json('hiCoders')
+  console.log(req.headers)
+  res.json({ message: 'hiCoders' })
 })
 
 app.listen(3000, () => console.log('Server running at port 3000'))
